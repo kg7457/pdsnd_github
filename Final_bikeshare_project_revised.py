8f35369b67a6a604b9b1b53a7506a03a9e69dc9d
Kasey-Godwin <kg7457@att.com>
2020-01-31 02:30:45 -0600

    Update data_display to raw_data 	modified:   Final_bikeshare_project_revised.py

--- Final_bikeshare_project_revised.py
+++ Final_bikeshare_project_revised.py
@@ -193,7 +193,7 @@ def user_stats(df):
     print('-'*40)
 
 
-def display_data(df):
+def display_raw_data(df):
     """Display data raw data"""
     
     start_loc = 0
@@ -222,7 +222,7 @@ def main():
             station_stats(df)
             trip_duration_stats(df)
             user_stats(df)
-            display_data(df)
+            display_raw_data(df)
             restart = input('\nWould you like to restart? Enter yes or no.\n')
             if restart.lower() != 'yes':
                 break
